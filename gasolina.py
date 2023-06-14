9bd7cfe2323216ae277ca74ffe7589c5a0660190
${GITHUB_USER} <${GITHUB_MAIL}>
2023-06-13 22:52:54 -0300

    Adicionar arquivos gerados: gasolina.csv, gasolina.py e gasolina.png

--- gasolina.py
+++ gasolina.py
@@ -3,3 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('gasolina.csv')
+df.plot(x='dia', y='venda')
+plt.xlabel('Dia')
+plt.ylabel('Preço')
+plt.savefig('gasolina.png')
+plt.show
